539476f43fa7846d7e08dc3e6760846fa7b2767c
Marouane Ahal <d144013152@gmail.com>
2022-04-21 18:14:22 +0000

    create url box

--- ui/src/components/home/UrlBox.jsx
+++ ui/src/components/home/UrlBox.jsx
@@ -1,11 +1,56 @@
-import React from 'react';
-
+import React from "react";
+import { useFormik } from "formik";
+import * as Yup from "yup";
 
 const UrlBox = () => {
-    return (
-        <div></div>
-    );
-};
+  const formik = useFormik({
+    initialValues: {
+      theUrl: "",
+    },
+    validationSchema: Yup.object({
+      theUrl: Yup.string().url("Enter a valid URL"),
+    }),
+    onSubmit: (values) => {
+      console.log(values);
+    },
+  });
 
+  return (
+    <div className="urlBox mx-auto text-center">
+      <h1>Paste the URL to be shortened</h1>
+      <form onSubmit={formik.handleSubmit}>
+        <div className="input-group">
+          <input
+            type="text"
+            name="theUrl"
+            id="theUrl"
+            placeholder="Enter the link here"
+            value={formik.values.theUrl}
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            className="form-control"
+          />
+          <div className="input-group-append">
+            <button type="submit" className="btn btn-primary">
+              Shorten URL
+            </button>
+          </div>
+        </div>
+        {formik.touched.theUrl && formik.errors.theUrl && (
+          <div className="text-start mt-2 text-sm text-danger">
+            {formik.errors.theUrl}
+          </div>
+        )}
+      </form>
+      <div className="parag text-center mx-auto">
+        <p className="first-p">Short URL is a free tool to shorten a URL or reduce a link</p>
+        <p>
+            Use our URL Shortener to create a shortened link making it easy to
+            remember
+        </p>
+      </div>
+    </div>
+  );
+};
 
 export default UrlBox;
--- ui/src/pages/TheHomePage.jsx
+++ ui/src/pages/TheHomePage.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
 
+import UrlBox from '../components/home/UrlBox';
 
 const TheHomePage = () => {
     return (
-        <main></main>
+        <div className='home container'>
+            <div className="row">
+                <div className="col-sm-10 mx-auto">
+                    <UrlBox />
+                </div>
+            </div>
+        </div>
     );
 };
 
