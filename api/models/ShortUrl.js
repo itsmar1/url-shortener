c9c6ddde7e71886bac8abc73624317560f21aec6
Marouane Ahal <d144013152@gmail.com>
2022-04-23 01:28:48 +0000

    create ShortUrl model

---
+++ api/models/ShortUrl.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+const shortid = require('shortid');
+
+const Schema = mongoose.Schema;
+
+const urlSchema = new Schema({
+    longUrl: {
+        type: String,
+        required: true
+    },
+    shortUrl: {
+        type: String,
+        required: true,
+        default: shortid.generate
+    },
+    clicks: {
+        type: Number,
+        required: true,
+        default: 0
+    }
+});
+
+
+module.exports = mongoose.model('ShortUrl', urlSchema);
